afd6b033311b0f6cc0884bbd1bf50f9c2f3c27a9
Venkata Krishna Garneni (Digital) <VE390515@wipro.com>
2018-05-13 13:22:57 +0530

    modified Calculator class

--- src/main/java/com/examples/entity/Calculator.java
+++ src/main/java/com/examples/entity/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
 	public int add(int i,int j){
 		return i+j;
 	}
+	public int subtract(int i,int j){
+		return i-j;
+	}
 	public static void main(String args[]){
 		Calculator c = new Calculator();
 		System.out.println("Sum is:"+c.add(2,3));
